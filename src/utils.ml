7f247ba2a6dfc0cb5783ce4caaaf47e1f459a043
Stephane Graham-Lengrand <stephane.graham-lengrand@sri.com>
2023-08-21 11:30:18 -0700

    Tracing with full arguments

--- src/utils.ml
+++ src/utils.ml
@@ -7,7 +7,8 @@ open Ext
 [%%if debug_mode]
 let print trace = Tracing.print Format.stdout trace
 [%%else]
-let print trace = Tracing.iprint Format.stdout trace
+(* let print trace = Tracing.iprint Format.stdout trace *)
+let print _trace _i fs = Format.ifprintf Format.stdout fs
 [%%endif]
 
 let pp_space fmt () = Format.fprintf fmt " @,"
